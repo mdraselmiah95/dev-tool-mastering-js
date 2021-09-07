10724016be0c2868a1579e69a36f1bac3f6656ae
rasel <russelroy15@gmail.com>
2021-09-07 14:22:27 +0600

    add new comment

--- dev-tool.js
+++ dev-tool.js
@@ -1,6 +1,14 @@
+// Tips for Making Your Website Accessible
+
+/* 
+Use the native HTML button tag instead of the ARIA role of button.
+Use the HTML label tag instead of aria-label or arial-labeledby.
+Use the HTML 5 nav tag instead of the ARIA role of navigation. 
+*/
+
 //dev tool learning
 
 //learn inside console
-console.log(object);
+console.log("regular log");
 console.error("user password is wrong");
 console.warn("Do not come in front of my special one");
