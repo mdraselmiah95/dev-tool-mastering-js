fbfd088f3a5442d3e8b767187e0f2d1e2a381662
rasel <russelroy15@gmail.com>
2021-09-07 14:37:52 +0600

    add new comment

--- dev-tool.js
+++ dev-tool.js
@@ -17,3 +17,5 @@ console.warn("Do not come in front of my special one");
 clear();
 
 //Console API reference   https://developer.chrome.com/docs/devtools/console/api/
+
+//https://developer.chrome.com/docs/devtools/console/utilities/
