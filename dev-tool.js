427bafcbe31064b081ce80ca6c7db4cf98de5d4d
rasel <russelroy15@gmail.com>
2021-09-07 14:25:16 +0600

    add new comment

--- dev-tool.js
+++ dev-tool.js
@@ -8,6 +8,8 @@ Use the HTML 5 nav tag instead of the ARIA role of navigation.
 
 //dev tool learning
 
+//To open console in Chrome  Windows: Ctrl + Shift + J
+
 //learn inside console
 console.log("regular log");
 console.error("user password is wrong");
